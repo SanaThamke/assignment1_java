b35b6555d0f12e3adf8066f0dcf8b01968164a97
SanaThamke <115568298+SanaThamke@users.noreply.github.com>
2022-10-25 16:12:10 +0530

    Add files via upload

---
+++ noPrint.java
@@ -0,0 +1,15 @@
+public class noPrint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu
+		int n =5;
+	      for (int i =1;i<n;i++) 
+	      {
+	    	  for (int j =1;j<n;j++) 
+	    	  {
+	    		  System.out.print(i);
+	    	  }
+	    	  System.out.println();
+	      }
+  }
+}
